0f00a5f142f6a386370f6b26ff7f5a4494de27ec
RamchandraPopatMulik <mulikram2016@gmail.com>
2022-11-04 13:22:20 +0530

    added file

---
+++ Generic1/MaximumNumber.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic1
+{
+    public class MaximumNumber
+    {
+        public int firstValue;
+        public int secondValue;
+        public int thirdValue;
+      
+        public MaximumNumber(int firstValue, int secondValue, int thirdValue)
+        {
+            this.firstValue = firstValue;
+            this.secondValue = secondValue;
+            this.thirdValue = thirdValue;
+        }
+
+        public static int MaximumIntegerNumber(int firstValue, int secondValue, int thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0 ||
+               firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) > 0 ||
+               firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) >= 0)
+            {
+                Console.WriteLine("Maximum Number is :" + firstValue);
+
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(firstValue) > 0 ||
+               secondValue.CompareTo(firstValue) >= 0 && secondValue.CompareTo(firstValue) > 0 ||
+               secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(firstValue) >= 0)
+            {
+                Console.WriteLine("Maximum Number is :" + secondValue);
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(firstValue) > 0 ||
+               thirdValue.CompareTo(firstValue) >= 0 && thirdValue.CompareTo(firstValue) > 0 ||
+               thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(firstValue) >= 0)
+            {
+                Console.WriteLine("Maximum Number is :" + thirdValue);
+            }
+            return firstValue;
+        }
+    }
+}
--- Generic1/Program.cs
+++ Generic1/Program.cs
@@ -1,36 +1,20 @@
 ﻿namespace Generic1
 {
-    public class PrintArray<T>
-    {
-        private  T[] inputArray;
-
-        public PrintArray(T[] inputArray)
-        {
-            this.inputArray = inputArray;
-        }
-        public void toPrint()
-        {
-            foreach (T item in inputArray)
-            {
-                Console.WriteLine(item);
-            }
-
-        }
-    }
+   
     internal class Program
     {
         static void Main(string[] args)
         {
-            int[] intArray = { 1, 2, 3, 4, 5, 6 };
-            double[] doubleArray = { 2.1, 2.2, 2.3, 2.4, 2.5, 2.6 };
-            char[] charArray = { 'a', 'b', 'c', 'd', 'e' };
+            Console.WriteLine("Please Enter First Value");
+            int firstValue = Convert.ToInt32(Console.ReadLine());
 
-           PrintArray<int>print= new PrintArray<int>(intArray);
-           print.toPrint();
-           PrintArray<double>print1= new PrintArray<double>(doubleArray);
-           print1.toPrint();
-           PrintArray<Char>print2=new PrintArray<char>(charArray);
-           print2.toPrint();
+            Console.WriteLine("Please Enter Second Value");
+            int secondValue = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please Enter Third Value");
+            int thirdValue = Convert.ToInt32(Console.ReadLine());
+
+            MaximumNumber.MaximumIntegerNumber(firstValue, secondValue, thirdValue);
         }
     }
 }
