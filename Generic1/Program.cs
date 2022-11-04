d2a466c842ed3426ab52c18cb4e62d747c8e40cb
RamchandraPopatMulik <mulikram2016@gmail.com>
2022-11-04 12:18:01 +0530

    added file

--- Generic1/Program.cs
+++ Generic1/Program.cs
@@ -1,8 +1,14 @@
 ﻿namespace Generic1
 {
-    internal class Program
+    public class PrintArray<T>
     {
-        public static void toPrint<T>(T[] inputArray)
+        private  T[] inputArray;
+
+        public PrintArray(T[] inputArray)
+        {
+            this.inputArray = inputArray;
+        }
+        public void toPrint()
         {
             foreach (T item in inputArray)
             {
@@ -10,14 +16,18 @@
             }
 
         }
+    }
+    internal class Program
+    {
         static void Main(string[] args)
         {
             int[] intArray = { 1, 2, 3, 4, 5, 6 };
             double[] doubleArray = { 2.1, 2.2, 2.3, 2.4, 2.5, 2.6 };
             char[] charArray = { 'a', 'b', 'c', 'd', 'e' };
-            Program.toPrint<int>(intArray);
-            Program.toPrint<double>(doubleArray);
-            Program.toPrint<char>(charArray);
+
+            new PrintArray<int>(intArray).toPrint();
+            new PrintArray<double>(doubleArray).toPrint();
+            new PrintArray<char>(charArray).toPrint();
         }
     }
 }
