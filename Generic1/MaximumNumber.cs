465fbfcb26bcffa89ba9b55abb8607201fc238e0
RamchandraPopatMulik <mulikram2016@gmail.com>
2022-11-07 12:56:23 +0530

    Added file

--- Generic1/MaximumNumber.cs
+++ Generic1/MaximumNumber.cs
@@ -26,17 +26,16 @@ namespace Generic1
                firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) >= 0)
             {
                 Console.WriteLine("Maximum Number is :" + firstValue);
-
             }
-            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(firstValue) > 0 ||
-               secondValue.CompareTo(firstValue) >= 0 && secondValue.CompareTo(firstValue) > 0 ||
-               secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(firstValue) >= 0)
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0 ||
+               secondValue.CompareTo(firstValue) >= 0 && secondValue.CompareTo(thirdValue) > 0 ||
+               secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) >= 0)
             {
                 Console.WriteLine("Maximum Number is :" + secondValue);
             }
-            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(firstValue) > 0 ||
-               thirdValue.CompareTo(firstValue) >= 0 && thirdValue.CompareTo(firstValue) > 0 ||
-               thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(firstValue) >= 0)
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0 ||
+               thirdValue.CompareTo(firstValue) >= 0 && thirdValue.CompareTo(secondValue) > 0 ||
+               thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) >= 0)
             {
                 Console.WriteLine("Maximum Number is :" + thirdValue);
             }
